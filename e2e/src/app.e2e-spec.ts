d9883b63d6253fa9e691d58588b6de7ef7a96f0a
Daniel Jungstedt <daniel.jungs@ibm.com>
2021-02-07 15:10:25 -0300

    change project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('frozen-sick app is running!');
+    expect(page.getTitleText()).toEqual('rpg-tracker app is running!');
   });
 
   afterEach(async () => {
