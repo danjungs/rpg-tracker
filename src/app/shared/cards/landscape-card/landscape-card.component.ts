492eefd0ba0c72ed072a11d909d73372f601d67a
Daniel Jungstedt <daniel.jungs@ibm.com>
2021-02-07 17:40:18 -0300

    create landscape-card component

---
+++ src/app/shared/cards/landscape-card/landscape-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-landscape-card',
+  templateUrl: './landscape-card.component.html',
+  styleUrls: ['./landscape-card.component.scss']
+})
+export class LandscapeCardComponent implements OnInit {
+  @Input() card;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.card)
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { ParalaxBannerComponent } from './paralax-banner/paralax-banner.component';
+import { LandscapeCardComponent } from './cards/landscape-card/landscape-card.component';
 
 const sharedComponents = [
   HeaderComponent,
+  ParalaxBannerComponent,
+  LandscapeCardComponent
 ]
 
 @NgModule({
