af3c373a36e394b6e7c5e0b1fa3f2eba568f4c49
Daniel Jungstedt <daniel.jungs@ibm.com>
2021-02-13 16:57:17 -0300

    add list and normal with title to text-element

---
+++ src/app/enums/text-area.enum.ts
@@ -0,0 +1,10 @@
+export enum TextEnum {
+  NORMAL = 'normal',
+  CARD = 'card',
+  READ_OUT_LOUD = 'read-out-loud',
+  FEATURE = 'feature',
+  TABLE = 'table',
+  RULE = 'rule',
+  TITLE = 'title',
+  LIST = 'list'
+}
